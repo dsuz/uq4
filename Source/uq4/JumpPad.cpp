d2776a4afd528b26eb61c0d5c33ac619121d08d4
Daisuke Suzuki <bboydaisuke@gmail.com>
2026-01-22 23:08:22 +0900

    中級編1 ジャンプパッドを追加した

--- Source/uq4/JumpPad.cpp
+++ Source/uq4/JumpPad.cpp
@@ -1,27 +1,65 @@
 #include "JumpPad.h"
 
+#include "UQ4Player.h"
+#include "Components/TimelineComponent.h"
+
 AJumpPad::AJumpPad()
 {
 	Frame = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
 	Pad = CreateDefaultSubobject<UStaticMeshComponent>("Pad");
 	Frame->SetupAttachment(RootComponent);
 	Pad->SetupAttachment(Frame);
+	BouncePadTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Bounce Timeline"));
+	BouncePadTimeline->SetLooping(false);
 }
 
 void AJumpPad::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	SetupTimeline();
 }
 
 void AJumpPad::OnStepOnPad(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent,
 	FVector NormalImpulse, const FHitResult& Hit)
 {
-	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Green, TEXT("Step on pad."));
+	this->Player = OtherActor;
+	BouncePadTimeline->PlayFromStart();
+}
+
+void AJumpPad::TimelineUpdate(float Value)
+{
+	Pad->SetRelativeLocation(FVector(0, 0.0f, Value));
+}
+
+void AJumpPad::TimelineFinished()
+{
+	auto Character = Cast<AUQ4Player>(Player);
+	if (Character)
+	{
+		auto LaunchVelocity = FVector(0, 0, Power);
+		Character->LaunchCharacter(LaunchVelocity, false, true);
+	}
 }
 
 void AJumpPad::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
 	Pad->OnComponentHit.AddDynamic(this, &AJumpPad::OnStepOnPad);
+}
+
+void AJumpPad::SetupTimeline()
+{
+	if (BouncePadTimelineCurve)
+	{
+		FOnTimelineFloat UpdateDelegate;
+		FOnTimelineEvent FinishDelegate;
+		UpdateDelegate.BindUFunction(this, FName("TimelineUpdate"));	// 文字列じゃなくて delegate で bind できないか？
+		FinishDelegate.BindUFunction(this, FName("TimelineFinished"));
+		BouncePadTimeline->AddInterpFloat(BouncePadTimelineCurve, UpdateDelegate);
+		BouncePadTimeline->SetTimelineFinishedFunc(FinishDelegate);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT("BouncePadTimelineCurve unassigned."));
+	}
 }
--- Source/uq4/JumpPad.h
+++ Source/uq4/JumpPad.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
 #include "GameFramework/Actor.h"
 #include "JumpPad.generated.h"
 
@@ -20,7 +21,21 @@ protected:
 	TObjectPtr<UStaticMeshComponent> Pad;
 	UFUNCTION()
 	void OnStepOnPad(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+	UPROPERTY()
+	TObjectPtr<UTimelineComponent> BouncePadTimeline;
+	UPROPERTY(EditAnywhere, Category=Timeline)
+	TObjectPtr<UCurveFloat> BouncePadTimelineCurve;
+	UPROPERTY(EditAnywhere)
+	float Power = 200;
+	UFUNCTION()
+	void TimelineUpdate(float Value);
+	UFUNCTION()
+	void TimelineFinished();
 
 public:
 	virtual void PostInitializeComponents() override;
+
+private:
+	void SetupTimeline();
+	TObjectPtr<AActor> Player;
 };
